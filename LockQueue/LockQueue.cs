43654c032dbb7277cc0a0a47f18bc56307728455
Maxim Odintsov <modintsov@issart.com>
2017-08-12 16:45:22 +0700

    Init LockQueue

---
+++ LockQueue/LockQueue.cs
@@ -0,0 +1,66 @@
+﻿using System.Collections.Generic;
+using System.Threading;
+
+namespace LockQueue
+{
+    public class LockQueue<T>
+    {
+        private readonly Queue<T> m_queue = new Queue<T>();
+        private bool m_stop;
+
+        public bool Push(T item)
+        {
+            if (m_stop)
+                return false;
+
+            lock (m_queue)
+            {
+                if (m_stop)
+                    return false;
+
+                m_queue.Enqueue(item);
+                Monitor.Pulse(m_queue);
+            }
+
+            return true;
+        }
+
+        public T Pop()
+        {
+            if (m_stop)
+                return default(T);
+
+            lock (m_queue)
+            {
+                if (m_stop)
+                    return default(T);
+
+                while (m_queue.Count == 0)
+                {
+                    Monitor.Wait(m_queue);
+                    if (m_stop)
+                        return default(T);
+                }
+
+                return m_queue.Dequeue();
+            }
+        }
+
+        public void Stop()
+        {
+            if (m_stop)
+                return;
+
+            lock (m_queue)
+            {
+                if (m_stop)
+                    return;
+
+                m_stop = true;
+                Monitor.PulseAll(m_queue);
+            }
+        }
+    }
+}
+
+
---
+++ LockQueue/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Threading;
+
+namespace LockQueue
+{
+    public class Program
+    {
+        private static readonly LockQueue<string> m_queue = new LockQueue<string>();
+
+        public static void Main(string[] args)
+        {
+            ThreadPool.QueueUserWorkItem(ReadThread);
+            ThreadPool.QueueUserWorkItem(ReadThread);
+
+            ThreadPool.QueueUserWorkItem(WriteThread);
+            ThreadPool.QueueUserWorkItem(WriteThread);
+            ThreadPool.QueueUserWorkItem(WriteThread);
+
+            Console.ReadLine();
+            m_queue.Stop();
+            Console.ReadLine();
+        }
+
+        private static void WriteThread(object arg)
+        {
+            for (int n = 1; n <= 10; n++)
+                m_queue.Push(Guid.NewGuid().ToString());
+            Console.WriteLine("WriteThread {0} thread finished.", Thread.CurrentThread.ManagedThreadId);
+        }
+
+        private static void ReadThread(object arg)
+        {
+            string item;
+            while ((item = m_queue.Pop()) != null)
+            {
+                Thread.Sleep(200);
+                Console.WriteLine("From ReadThread {0}, value {1}", Thread.CurrentThread.ManagedThreadId, item);
+            }
+            Console.WriteLine("ReadThread {0} thread finished.", Thread.CurrentThread.ManagedThreadId);
+        }
+    }
+}
+
+
+
+
